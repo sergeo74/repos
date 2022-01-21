feb79d786bbea8a4acc8437601b4ce24d830036e
Сергей Нехаенко <sergeo74@outlook.com>
2022-01-21 20:02:32 +0300

    21.01.2021

---
+++ Troelsen/Chapter 5/SimpleClassExample/SimpleClassExample/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Car
+    {
+        // 'Состояние’ объекта Саг
+        public string petName;
+        public int currSpeed;
+
+        // Функциональность Саг.
+        // Использовать синтаксис членов, сжатых до выражений, который появился в C# 6.
+        public void PrintState() => 
+            Console.WriteLine("{0} is going {1} MPH.", petName, currSpeed);
+        public void SpeedUp(int delta) => 
+            currSpeed =+delta;
+    }
+}
