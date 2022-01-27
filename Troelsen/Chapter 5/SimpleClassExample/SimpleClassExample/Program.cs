89c76ac847c8175872a2935f4b712360feffee82
Сергей Нехаенко <sergeo74@outlook.com>
2022-01-27 16:22:15 +0300

    1

--- Troelsen/Chapter 5/SimpleClassExample/SimpleClassExample/Program.cs
+++ Troelsen/Chapter 5/SimpleClassExample/SimpleClassExample/Program.cs
@@ -6,7 +6,11 @@ namespace SimpleClassExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("****Fun with Simple Class!****");
+            var myCar = new Car();
+            myCar.petName = "Henry";
+            myCar.currSpeed = 10;
+            myCar.PrintState();
         }
     }
 }
